e5b4893561df1ba55bd2ff40bd16d36f2f58ce5a
nao10 <nao10.iwahashi@gmail.com>
2024-11-03 14:38:53 +0900

    6章まで完了

---
+++ app/_components/Sheet/index.tsx
@@ -0,0 +1,9 @@
+import { isValid } from "date-fns";
+import styles from "./index.module.css";
+
+type Props = {
+    children: React.ReactNode;
+};
+export default function Sheet({ children }: Props) {
+    return <div className={styles.container}>{children}</div>;
+}
